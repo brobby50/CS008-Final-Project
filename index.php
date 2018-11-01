0708d1176e85606136b0931be6b7b38c2ede13cd
brobby50 <41029325+brobby50@users.noreply.github.com>
2018-11-01 11:35:01 -0400

    Update index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Climate Change</title>
+
+        <meta charset="utf-8">
+        <meta name="author" content="Robert And Cole">
+        <meta name="description" content="description">
+
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        
+
+    </head>
+<article> 
+    
+    </figure>
+    <h2>Final Project</h2>
+
+    <p>Description</p>
+    </article>
+
+</body>
+</html>
