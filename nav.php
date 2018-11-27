c9641db1e679840d066ca81bcf917786309c961b
brobby50 <41029325+brobby50@users.noreply.github.com>
2018-11-27 11:46:05 -0500

    drop down box

--- nav.php
+++ nav.php
@@ -2,7 +2,7 @@
 <nav>
     <ol>
         <?php
-      
+        
         print '<li class="';
         if ($path_parts['filename'] == "index") {
             print ' activePage ';
@@ -17,7 +17,22 @@
         }
         print '">';
         print '<a href="tutorial.php">Tutorial</a>';
-        print '</li>';
+        print '</li>';      
         ?>
     </ol>
+    <?php $page = "Home"; ?>
+    <fieldset  class="listbox">
+                <legend>Page</legend>
+                <p>
+                    <select id="lstPage" 
+                            name="lstPage" 
+                            tabindex="520" >
+                        <option <?php if ($page == "Home") print " selected "; ?>
+                            value="Vanilla"> Home</option>
+
+                        <option <?php if ($page == "Tutorial") print " selected "; ?>
+                            value="Chocolate"> Tutorial</option>
+                    </select>
+                </p>
+            </fieldset><!-- ends Drop down list -->
 </nav>
