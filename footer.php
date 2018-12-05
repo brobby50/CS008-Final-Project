f8db5ce6bcc72d254cec4400922be2ce2e1192aa
brobby50 <41029325+brobby50@users.noreply.github.com>
2018-12-05 12:03:28 -0500

    boi

--- footer.php
+++ footer.php
@@ -1,4 +1,5 @@
 <!-- /////////////////////    Footer   ///////////////////// -->
 <footer>
     <p>Designed by Robby and Cole</p>
+    <p><a href="SpecificationDocument.pdf">Specification Document</a></p>
 </footer>
